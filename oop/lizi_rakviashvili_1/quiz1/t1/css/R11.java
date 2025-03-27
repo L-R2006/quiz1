ef4b55005f42324f9dc6929e3865974a31dad3b1
L-R2006 <lizi.rakviashvili.1@iliauni.edu.ge>
2025-03-27 10:18:55 +0400

    Add files via upload

---
+++ oop/lizi_rakviashvili_1/quiz1/t1/css/R11.java
@@ -0,0 +1,8 @@
+package lizi_rakviashvili_1.quiz1.t1.css;
+import lizi_rakviashvili_1.quiz1.t1.plugin.R12;
+
+public class R11 {
+    public static void main(String[] args) {
+        System.out.println(R12.MESSAGE); 
+}
+}
